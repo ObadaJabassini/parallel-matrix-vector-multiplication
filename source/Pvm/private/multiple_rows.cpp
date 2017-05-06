91a2f46c7a65fbd992e3ca87affb700db104b0c6
Obada <obj1995@hotmail.com>
2017-05-07 00:18:13 +0300

    Initial Commit

--- source/Pvm/private/multiple_rows.cpp
+++ source/Pvm/private/multiple_rows.cpp
@@ -7,109 +7,147 @@
 
 using namespace std;
 
-//class NotEnoughParameterException : public exception{
-//public:
-//    const char* what() const override {
-//        return "Not Enough parameters";
-//    }
-//};
-
-int main(int argc, char** argv) {
-    char* groupName = "multiple_row";
-    double** matrix;
+int main( int argc, char** argv ) {
+    char* groupName = "multiple_rows";
     double* vector;
-    pvm_catchout(stdout);
+    double* part;
+    double* matrix;
+    pvm_catchout( stdout );
     int tid = pvm_mytid();
     int parent_tid = pvm_parent();
     bool is_parent = (parent_tid == PvmNoParent) || (parent_tid == PvmParentNotSet);
-    int size, count, offset, rows, cols, num_of_process;
-    int* child_id;
     auto start = chrono::steady_clock::now();
+    int rows, cols, index;
+    int block_size;
+    int* others;
+    int count;
     if ( is_parent ) {
+        index = 0;
+        block_size = stoi( argv[3] );
+        double* temp;
+        double** matrix;
         auto reader = make_shared<TextDataReader>();
-        size = reader->read(argv[1], matrix, vector);
-        count = size - 1;
-        offset = atoi(argv[3]);
-        child_id = new int[count];
+        cols = reader->read( argv[1],
+                             matrix,
+                             vector );
+        rows = cols;
+        if ( cols % block_size != 0 ) {
+            rows = cols + cols % block_size;
+            temp = new double[rows * cols];
+            for ( int i = 0; i < cols; ++i ) {
+                for ( int j = 0; j < cols; ++j ) {
+                    temp[i * cols + j] = matrix[i][j];
+                }
+            }
+            for ( int i = cols; i < rows; ++i ) {
+                temp[i] = 0;
+            }
+            double* temp2 = new double[rows];
+            for ( int k = 0; k < cols; ++k ) {
+                temp2[k] = vector[k];
+            }
+            for ( int l = cols; l < rows; ++l ) {
+                temp2[l] = 0;
+            }
+            vector = temp2;
+        } else {
+            temp = new double[cols * cols];
+        }
         start = chrono::steady_clock::now();
-        rows = size + size % offset;
-        cols = size;
-        num_of_process = rows / offset;
-        int cc = pvm_spawn("/home/ojabassini/CLionProjects/parallel-matrix-vector-multiplication/pvm/multiple_rows", nullptr, 0, "", num_of_process - 1,
-                           child_id);
-        if ( cc != num_of_process - 1 ) {
-            cout << "\nFailed to spwan required children\n...Exit...Press any Key to exit\n";
+        count = rows / block_size;
+        others = new int[count - 1];
+        int cc = pvm_spawn( "/home/ojabassini/CLionProjects/parallel-matrix-vector-multiplication/bin/multiple_rows",
+                            NULL,
+                            0,
+                            "",
+                            count - 1,
+                            others );
+        if ( cc != count - 1 ) {
+            cout << "\nFailed to spawn required children \n...Exit...Press any Key to exit\n";
             pvm_exit();
+            exit( -1 );
         }
-        pvm_initsend(PvmDataDefault);
-        pvm_pkint(&size, 1, 1);
-        pvm_pkint(&count, 1, 1);
-        pvm_pkint(&offset, 1, 1);
-        pvm_pkdouble(vector, size, 1);
-        pvm_mcast(child_id, count, 1);
-        if ( size % offset != 0 ) {
-            double** temp = new double* [size + size % offset];
-            for ( int l = 0; l < size + size % offset; ++l ) {
-                temp[l] = new double[size];
-            }
-            for ( int i = 0; i < size; ++i ) {
-                for ( int j = 0; j < size; ++j ) {
-                    temp[i][j] = matrix[i][j];
-                }
-            }
-            for ( int k = size; k < size + size % offset; ++k ) {
-                for ( int i = 0; i < size; ++i ) {
-                    temp[k][i] = 0;
-                }
-            }
-            matrix = temp;
+        for ( int m = 1; m < count; ++m ) {
+            pvm_initsend( PvmDataDefault );
+            pvm_pkint( &rows, 1, 1 );
+            pvm_pkint( &cols, 1, 1 );
+            pvm_pkint( &block_size, 1, 1 );
+            pvm_pkint( &count, 1, 1 );
+            pvm_pkdouble( temp + m * cols * block_size, cols * block_size, 1 );
+            pvm_pkdouble( vector + m * block_size, block_size, 1 );
+            pvm_send( others[m - 1], m );
+        }
+        index = 0;
+        part = new double[block_size];
+        for ( int n = 0; n < block_size; ++n ) {
+            part[n] = vector[n];
         }
     } else {
-        pvm_recv(parent_tid, 1);
-        pvm_upkint(&size, 1, 1);
-        pvm_upkint(&count, 1, 1);
-        pvm_upkint(&offset, 1, 1);
-        pvm_upkdouble(vector, size, 1);
-        rows = size + size % offset;
-        cols = size;
-        num_of_process = num_of_process = rows / offset;
+        int buffer = pvm_recv( -1, -1 );
+        pvm_bufinfo( buffer, NULL, &index, NULL );
+        pvm_upkint( &rows, 1, 1 );
+        pvm_upkint( &cols, 1, 1 );
+        pvm_upkint( &block_size, 1, 1 );
+        pvm_upkint( &count, 1, 1 );
+        matrix = new double[block_size * cols];
+        pvm_upkdouble( matrix, cols * block_size, 1 );
+        part = new double[block_size];
+        pvm_upkdouble( part, block_size, 1 );
+        others = new int[count - 1];
+        others[0] = parent_tid;
+        int* sib = new int[count - 1];
+        pvm_siblings( &sib );
+        for ( int i = 0, j = 1; i < count - 1; ++i ) {
+            if ( tid != sib[i] )
+                others[j++] = sib[i];
+        }
     }
-    int gid = pvm_joingroup(groupName);
-    pvm_barrier(groupName, num_of_process);
-    int groupSize = pvm_gsize(groupName);
-    int allRoot;
-    if ( is_parent )
-        allRoot = pvm_getinst(groupName, tid);
-    else
-        allRoot = pvm_getinst(groupName, parent_tid);
-    int* rec = new int[size * offset];
+    vector = new double[rows];
+    for ( int i = 0; i < block_size; ++i ) {
+        vector[index * block_size + i] = part[i];
+    }
+    pvm_initsend( PvmDataDefault );
+    pvm_pkdouble( part, block_size, 1 );
+    pvm_mcast( others, count - 1, index );
+    for ( int l = 0; l < count - 1; ++l ) {
+        int temp;
+        int buffer = pvm_recv( -1,
+                               -1 );
+        pvm_bufinfo( buffer, NULL, &temp, NULL );
+        pvm_upkdouble( part, block_size, 1 );
+        for ( int i = 0; i < block_size; ++i ) {
+            vector[temp * block_size + i] = part[i];
+        }
+    }
+    double* result = new double[block_size];
+    for ( int i = 0; i < block_size; ++i ) {
+        result[i] = 0;
+        for ( int j = 0; j < cols; ++j ) {
+            result[i] += matrix[i * cols + j] * vector[j];
+        }
+    }
     if ( is_parent ) {
-        auto scatterData = new double[rows * cols];
-        for ( int i = 0; i < size; ++i ) {
-            for ( int j = 0; j < size; ++j ) {
-                scatterData[size * i + j] = matrix[i][j];
+        for ( int i = 0; i < block_size; ++i ) {
+            vector[i] = result[i];
+        }
+        for ( int i = 0; i < count - 1; ++i ) {
+            int buffer = pvm_recv( -1, -1 );
+            pvm_bufinfo( buffer, NULL, &index, NULL );
+            pvm_upkdouble( result, block_size, 1 );
+            for ( int j = 0; j < block_size; ++j ) {
+                vector[index * block_size + j] = result[j];
             }
         }
-        pvm_scatter(rec, scatterData, size * offset, PVM_DOUBLE, 2, groupName, allRoot);
-    } else {
-        pvm_scatter(rec, NULL, size * offset, PVM_DOUBLE, 2, groupName, allRoot);
-    }
-    auto* send = new double[offset];
-    for ( int m = 0; m < offset; ++m ) {
-        send[m] = 0;
-        for ( int i = 0; i < cols; ++i ) {
-            send[m] += rec[m * cols + i] * vector[i];
+        for ( int k = 0; k < rows; ++k ) {
+            cout << vector[k] << " ";
         }
-    }
-    if ( !is_parent )
-        pvm_gather(NULL, send, offset, PVM_DOUBLE, 3, groupName, allRoot);
-    else {
-        auto result = new double[rows];
-        pvm_gather(result, send, offset, PVM_DOUBLE, 3, groupName, allRoot);
         auto end = chrono::steady_clock::now();
-        TextDataWriter().write(argv[2], result, size, chrono::duration<double, milli>(end - start).count());
+        TextDataWriter().write( argv[2], vector, rows - rows % block_size,
+                                chrono::duration<double, milli>( end - start ).count());
+    } else {
+        pvm_initsend( PvmDataDefault );
+        pvm_pkdouble( result, block_size, 1 );
+        pvm_send( parent_tid, index );
     }
-    pvm_barrier(groupName, groupSize);
-    pvm_lvgroup(groupName);
     pvm_exit();
 }
