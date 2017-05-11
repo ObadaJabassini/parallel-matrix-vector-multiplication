372f2445259b099e603c7d8127e64ba9c85ce6cc
Obada <obj1995@hotmail.com>
2017-05-11 18:43:42 +0300

    Finish

--- include/RandomGenerator/RubbishRandomGenerator.h
+++ include/RandomGenerator/RubbishRandomGenerator.h
@@ -2,23 +2,20 @@
 // Created by ojabassini on 5/11/17.
 //
 
-#ifndef PROJECT_RUBBISHRANDOMGENERATOR_H
-#define PROJECT_RUBBISHRANDOMGENERATOR_H
+#ifndef PROJECT_RUBBISH_H
+#define PROJECT_RUBBISH_H
 
-#include <memory>
-#include <random>
-#include "RandomGenerator.h
+#include "RandomGenerator.h"
 
-namespace Generator {
-    class RubbishRandomGenerator : public RandomGenerator {
+namespace Generator{
+ class RubbishRandomGenerator : public RandomGenerator{
     public:
-        double generate() override;
+       double generate() override;
 
-        void generate( double*& arr, int size ) override;
+       void generate( double*& arr, int size ) override;
 
-        void generate( double**& mat, int rows, int cols ) override;
-    };
+       void generate( double**& mat, int rows, int cols ) override;
+ };
 }
 
-
-#endif //PROJECT_RUBBISHRANDOMGENERATOR_H
+#endif //PROJECT_RUBBISH_H
