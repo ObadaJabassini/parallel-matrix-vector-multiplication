3919fb6dee839224570125da49dacc5b71889f13
Obada <obj1995@hotmail.com>
2017-05-19 16:23:10 +0300

    Finish

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 #include <memory>
-#include <RandomGenerator/NormalRandomGenerator.h>
-#include <DataHandler/TextDataReader.h>
-#include <RandomGenerator/UniformRandomGenerator.h>
-#include <Mpi/SingleRowMultiplier.h>
-#include <Performance/Benchmarker.h>
-#include <QtWidgets/QPushButton>
 #include <QtWidgets/QApplication>
 #include <QtCore/QTextStream>
 #include <QtCore/QFile>
-#include <QtCore/QObject>
 #include <Ui/mainwindow.h>
-#include "../../../qcustomplot/qcustomplot.h"
 
 using namespace std;
-using namespace Generator;
-using namespace DataHandler;
-using namespace Mpi;
-using namespace Performance;
 
 int main( int argc, char** argv ) {
     QApplication app( argc, argv );
